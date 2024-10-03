27b8ed2d83eece81600c1f5b00b3122c407afe4b
dawidzurawski <dawidzurawskidrz@gmail.com>
2024-10-03 05:21:25 +0100

    Conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const name = 5 === 6
+  const isNameShowing = 5 === 6
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello, {isNameShowing ? 9 : 'someone'}!</h1>
+      {name ? (
+        <>
+          test
+        </>) : (
+          <>
+            <h1>Hello</h1>
+            <h2>Blue</h2>
+          </>
+        )}
     </div>
   );
 }
