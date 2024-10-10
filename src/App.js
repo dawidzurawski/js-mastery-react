6ab67920eb19e3d6a1630911b6716ef40f30ab56
dawidzurawski <dawidzurawskidrz@gmail.com>
2024-10-10 21:14:57 +0100

    Fix fetching issue

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css'
 import MovieCard from './MovieCard'
 import SearchIcon from './search.svg'
 
-const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=a4f13c42'
+const API_URL = 'https://www.omdbapi.com/?i=tt3896198&apikey=a4f13c42'
 
 const App = () => {
   const [movies, setMovies] = useState([])
